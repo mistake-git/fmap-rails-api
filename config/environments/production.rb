f3c19f7af02dc572fad2e8f7de824e8c6b14ec63
mistake-git <acbmstk0402@gmail.com>
2020-12-22 13:01:30 +0900

    本番環境のプロトコルを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  Rails.application.routes.default_url_options[:host] = 'https://fmap-api.tk'
+  Rails.application.routes.default_url_options[:host] = 'fmap-api.tk'
   Rails.application.routes.default_url_options[:port] = 3000
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
