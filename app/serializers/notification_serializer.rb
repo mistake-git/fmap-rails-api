6a210e1a46150f51e136c841dcdc33d47d4f848a
mistake-git <acbmstk0402@gmail.com>
2021-02-14 04:52:50 +0900

    [add]リレーションの追加

---
+++ app/serializers/notification_serializer.rb
@@ -0,0 +1,7 @@
+class NotificationSerializer < ActiveModel::Serializer
+  attributes :id, :post, :comment, :visitor, created_at, :action
+  belongs_to :post, serializer: PostSerializer
+  belongs_to :comment, serializer: CommentSerializer
+  belongs_to :visitor, serializer: UserSerializer
+  belongs_to :visited, serializer: UserSerializer
+end
