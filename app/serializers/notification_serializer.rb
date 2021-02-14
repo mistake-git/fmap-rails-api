79f2fb4279e7b31e7170501fb534386d92070b65
mistake-git <acbmstk0402@gmail.com>
2021-02-14 15:02:39 +0900

    [fix]通知のslializerを修正

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,5 +1,5 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id, :post, :comment, :visitor, created_at, :action
+  attributes :id, :post, :comment, :visitor, :created_at, :action
   belongs_to :post, serializer: PostSerializer
   belongs_to :comment, serializer: CommentSerializer
   belongs_to :visitor, serializer: UserSerializer
